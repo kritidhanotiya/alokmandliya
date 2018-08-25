194d6aab0d68e4c192b5bb1e821425afb7ef6a57
Unknown <kriti.dhanotiya@gmail.com>
2018-08-25 09:53:15 +0530

    Initial Working version

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import request from 'request';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 import {
-  Nav, Navbar, NavItem, Col, Image, Row, Grid, Button, Modal, ListGroup, ListGroupItem, Form, FormControl, FormGroup, Panel
+  Nav, Navbar, NavItem, Col, Image, Row, Grid, Button, Modal, ListGroup, ListGroupItem, Form, FormControl, FormGroup, Panel, PageHeader
 } from 'react-bootstrap';
 import MaskedFormControl from 'react-bootstrap-maskedinput';
 import 'react-datepicker/dist/react-datepicker.css';
+import FacebookLogin from 'react-facebook-login';
 import logo from './alok_mandliya.jpg';
 import './App.css';
 
@@ -25,7 +26,10 @@ class App extends Component {
       selectedDate: moment(),
       selectedTime: moment(),
       bookAppointmentshow: false,
-      contactshow: false
+      contactshow: false,
+      loggedIn: true,
+      user: JSON.parse('{"name":"Nitesh Gupta","email":"niteshgusty@rediffmail.com","picture":{"data":{"height":50,"is_silhouette":false,"url":"https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=2065981463454429&height=50&width=50&ext=1537250854&hash=AeRNTX-L0rZFLfge","width":50}},"id":"2065981463454429","accessToken":"EAAO4owD1rXQBAIQpYAhzOZCt7hZAGXkjzxfEhIaZA1if1tlsKt4n4nb3BzizFjY6ixZBg1HouC1rTAxpKeuc6CNcebWHC9S9vJ3ZA5g9QLr1TAeWp2JMRH65RtCI125P5F1Pu71CR8EL70isjZC0miI28Q42Qo2Xukaqe4AchmWZCsOXSRkxiSsGW4vxEHy1tVzWJT2WD2bSgZDZD","userID":"2065981463454429","expiresIn":6747,"signedRequest":"lbddNy-dy-fQ-XDSB-cj0kRc0DnmJKLmcVwYKcI_4uo.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImNvZGUiOiJBUUJPV2JfeTNFMV9ENW92TjZRY29EWVZtcDkzR0ltc0dtdWNfa2l0d1pyZzVkbVFkMWtfaVpPNEw4YmVPYzl2VGxud0g1ZGktVHFEY1BabW5CV25RQVo3STNJRS1LTmt5a1Q4d0p5UFRCcEFWWkIxVWZ3SC1Kb0hUd3R3MHVxT0RNQi1HRlJka3gzel9wamJOQ05xMFdrYWR3eVFMbG1ySWJNV001SC14UGY5ajZKSTk1akEtbWVzZ0VDQ2ExTjdSOVQ2cFRJbzBpWlNWYVo1ajNQX1V2TEN1V3lYRXBoNExEdkFhUWZuRzBoUGVFa3Q1QUJTVUdESW1kQmFWOUZXR3NCSG5KcGJqYjBLSklZUk5GTkE4SWRscDd1WWtGbmZqUzV3RzlvMktEbGp2WEx0Q25aUFUyTWlFWVZmMFQ2UzlKZ3hMcHhIWDdMWXJybkxLd1k0VDRtTTRHX3ZHWUdLaFhHYWpjNjd5d1B5MlEiLCJpc3N1ZWRfYXQiOjE1MzQ2NTg4NTMsInVzZXJfaWQiOiIyMDY1OTgxNDYzNDU0NDI5In0","reauthorize_required_in":7775142}'),
+      userShow: false
     };
   }
 
@@ -75,30 +79,54 @@ class App extends Component {
   handleContactClose = () => {
     this.setState({ contactshow: false });
   }
-
+  handleUserClose = () => {
+    this.setState({ userShow: false });
+  }
   handleBookAppointmentShow = () => {
     this.setState({ bookAppointmentshow: true });
   }
   handleContactShow = () => {
     this.setState({ contactshow: true });
   }
+  handleUserShow = () => {
+    this.setState({ userShow: true });
+  }
+  isWeekday = (date) => {
+    const day = date.day()
+    return day !== 0
+  }
   handleConfirmAppointment = () => {
+
     var post_data = JSON.stringify({
       name: this.state.selectedName,
       email: this.state.selectedEmail,
       mobile: this.state.selectedPhoneNumber,
-      date: this.state.selectedDate,
-      time: this.state.selectedTime
+      date: moment(this.state.selectedDate).format('YYYY-MM-DD'),
+      time: moment(this.state.selectedTime).format("HH:mm")
     });
 
     request.post({
-      headers: {  'Content-Type': 'application/json' },
-      url: 'http://localhost:8000/api/appointments?access_token=abc123',
+      headers: { 'Content-Type': 'application/json' },
+      url: 'https://apiserver-211007.appspot.com/api/appointments?access_token=11993245667',
       body: post_data
-    }, function (error, response, body) {
-      console.log(body);
+    }, (error, response, body) => {
+      if (response.statusCode === 200) {
+        this.setState({
+          selectedPage: 3
+        });
+      }
     });
 
+
+  }
+
+  responseFacebook = (response) => {
+    console.log(response);
+    if(response.name) {
+      this.setState({loggedIn: true, user: response});
+    }
+  }
+  componentClicked = () => {
 
   }
 
@@ -126,7 +154,25 @@ class App extends Component {
               <NavItem eventKey={2} href="#" onClick={this.handleContactShow}>
                 <i className="fa fa-envelope fa-fw"></i> Contact Us
               </NavItem>
-
+              <NavItem href="#">
+               {!this.state.loggedIn && <FacebookLogin
+                  appId="1047435165412724"
+                  autoLoad={true}
+                  cssClass="facebook-button btn btn-danger"
+                  fields="name,email,picture"
+                  disableMobileRedirect={true}
+                  redirectUri="https://alokmandliya.com"
+                  onClick={this.componentClicked}
+                  callback={this.responseFacebook} />
+               }
+               {this.state.loggedIn &&
+                <NavItem href="#" onClick={this.handleUserShow}>
+                <span className="user-profile">
+                <img src={this.state.user.picture.data.url} /> {this.state.user.name}
+                </span>
+              </NavItem>
+               }
+              </NavItem>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
@@ -158,7 +204,7 @@ class App extends Component {
                   <table align="center">
                     <tr>
                       <td>
-                        <i class="fa fa-capsules"></i></td>
+                        <i className="fa fa-capsules"></i></td>
                       <td>
                         &nbsp;MD (Medicine) from Seth GS Medical college and KEM Hospital Mumbai 2007-2010
                          </td>
@@ -300,9 +346,21 @@ class App extends Component {
                   <FormGroup><MaskedFormControl type='text' name='phoneNumber' placeholder="Phone number" mask='1111111111' onChange={(e) => this.handlePhoneNumberChange(e)} />
                   </FormGroup>
                   <FormGroup>
-                    <Button bsStyle="success" onClick={this.handleConfirmAppointment}>Confirm Appointment</Button>
+                    <Button bsStyle="success" onClick={() => this.handleConfirmAppointment()}>Confirm Appointment</Button>
                   </FormGroup>
                 </Form>
+              </div>
+            }
+            {this.state.selectedPage === 3 &&
+              <div>
+                <PageHeader>
+                  Your Appointment is Booked
+                  <p><small>
+                    Date: {moment(this.state.selectedDate).format('DD-MM-YYYY')}
+                    &nbsp; Time: {moment(this.state.selectedTime).format("HH:mm")}
+                  </small>
+                  </p>
+                </PageHeader>
               </div>
             }
 
@@ -316,22 +374,36 @@ class App extends Component {
             </div>
           </Modal.Footer>
         </Modal>
+
         <Modal show={this.state.contactshow} onHide={this.handleContactClose}>
           <Modal.Header closeButton>
             <Modal.Title>Contact Us</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            {this.state.selectedPage === 1 &&
-              <div>
-                <h4>Dr Alok Mandliya</h4>
-                <h5>Bombay Hospital Indore</h5>
-                <h6>dralokdm@gmail.com</h6>
-                <h6>9754086504</h6>
-              </div>
-            }
+            <div>
+              <h4>Dr Alok Mandliya</h4>
+              <h5>Bombay Hospital Indore</h5>
+              <h6><a href={"mailto: dralokdm@gmail.com"}>dralokdm@gmail.com</a></h6>
+              <h6><a href="tel:9754086504">9754086504</a></h6>
+            </div>
           </Modal.Body>
           <Modal.Footer>
             <Button bsStyle="danger" onClick={this.handleContactClose}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+
+        <Modal show={this.state.userShow} onHide={this.handleContactClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Profile</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div>
+              <h4>{this.state.user.name}</h4>
+              <h5>{this.state.user.email}</h5>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="danger" onClick={this.handleUserClose}>Close</Button>
           </Modal.Footer>
         </Modal>
       </div>
