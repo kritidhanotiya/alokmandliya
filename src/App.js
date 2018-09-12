4da31c3c3faa834eaf8b9d0c9166a75344e75890
Unknown <kriti.dhanotiya@gmail.com>
2018-09-12 23:57:28 +0530

    added date time checks

--- src/App.js
+++ src/App.js
@@ -23,13 +23,14 @@ class App extends Component {
       selectedName: '',
       selectedEmail: '',
       selectedPhoneNumber: '',
-      selectedDate: (moment().format("dd")=="Sunday") ? moment().add('days', 1): moment(),
+      selectedDate: (moment().format("dd") == "Sunday") ? moment().add('days', 1) : moment(),
       selectedTime: moment(),
       bookAppointmentshow: false,
       contactshow: false,
       loggedIn: true,
       bookedAppointments: [],
       bookedSlots: [],
+      errors: {},
       user: JSON.parse('{"name":"Nitesh Gupta","email":"niteshgusty@rediffmail.com","picture":{"data":{"height":50,"is_silhouette":false,"url":"https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=2065981463454429&height=50&width=50&ext=1537250854&hash=AeRNTX-L0rZFLfge","width":50}},"id":"2065981463454429","accessToken":"EAAO4owD1rXQBAIQpYAhzOZCt7hZAGXkjzxfEhIaZA1if1tlsKt4n4nb3BzizFjY6ixZBg1HouC1rTAxpKeuc6CNcebWHC9S9vJ3ZA5g9QLr1TAeWp2JMRH65RtCI125P5F1Pu71CR8EL70isjZC0miI28Q42Qo2Xukaqe4AchmWZCsOXSRkxiSsGW4vxEHy1tVzWJT2WD2bSgZDZD","userID":"2065981463454429","expiresIn":6747,"signedRequest":"lbddNy-dy-fQ-XDSB-cj0kRc0DnmJKLmcVwYKcI_4uo.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImNvZGUiOiJBUUJPV2JfeTNFMV9ENW92TjZRY29EWVZtcDkzR0ltc0dtdWNfa2l0d1pyZzVkbVFkMWtfaVpPNEw4YmVPYzl2VGxud0g1ZGktVHFEY1BabW5CV25RQVo3STNJRS1LTmt5a1Q4d0p5UFRCcEFWWkIxVWZ3SC1Kb0hUd3R3MHVxT0RNQi1HRlJka3gzel9wamJOQ05xMFdrYWR3eVFMbG1ySWJNV001SC14UGY5ajZKSTk1akEtbWVzZ0VDQ2ExTjdSOVQ2cFRJbzBpWlNWYVo1ajNQX1V2TEN1V3lYRXBoNExEdkFhUWZuRzBoUGVFa3Q1QUJTVUdESW1kQmFWOUZXR3NCSG5KcGJqYjBLSklZUk5GTkE4SWRscDd1WWtGbmZqUzV3RzlvMktEbGp2WEx0Q25aUFUyTWlFWVZmMFQ2UzlKZ3hMcHhIWDdMWXJybkxLd1k0VDRtTTRHX3ZHWUdLaFhHYWpjNjd5d1B5MlEiLCJpc3N1ZWRfYXQiOjE1MzQ2NTg4NTMsInVzZXJfaWQiOiIyMDY1OTgxNDYzNDU0NDI5In0","reauthorize_required_in":7775142}'),
       userShow: false
     };
@@ -44,9 +45,11 @@ class App extends Component {
             const appointments = JSON.parse(body);
             const date = this.state.selectedDate;
             const bookedAppointments = appointments.map(appointment => { return moment(appointment.date + 'T' + appointment.time) });
-            this.setState({ bookedAppointments,
-            bookedSlots: bookedAppointments.filter(appointment => { return moment(appointment).format('YYYY-MM-DD') === date.format('YYYY-MM-DD') } )
-            .map(appointment => { return moment(appointment) }) })
+            this.setState({
+              bookedAppointments,
+              bookedSlots: bookedAppointments.filter(appointment => { return moment(appointment).format('YYYY-MM-DD') === date.format('YYYY-MM-DD') })
+                .map(appointment => { return moment(appointment) })
+            })
           }
         }
       )
@@ -56,8 +59,8 @@ class App extends Component {
     const bookedAppointments = JSON.parse(JSON.stringify(this.state.bookedAppointments));
     this.setState({
       selectedDate: date,
-      bookedSlots: bookedAppointments.filter(appointment => { return moment(appointment).format('YYYY-MM-DD') === date.format('YYYY-MM-DD') } )
-      .map(appointment => { return moment(appointment) })
+      bookedSlots: bookedAppointments.filter(appointment => { return moment(appointment).format('YYYY-MM-DD') === date.format('YYYY-MM-DD') })
+        .map(appointment => { return moment(appointment) })
     });
   }
   handleTimeChange = (time) => {
@@ -66,6 +69,7 @@ class App extends Component {
     });
   }
   handleNameChange = (e) => {
+
     this.setState({
       selectedName: e.target.value
     });
@@ -117,27 +121,73 @@ class App extends Component {
     const day = date.day()
     return day !== 0
   }
-  handleConfirmAppointment = () => {
+  handleValidation() {
+    let errors = {};
+    let formIsValid = true;
 
-    var post_data = JSON.stringify({
-      name: this.state.selectedName,
-      email: this.state.selectedEmail,
-      mobile: this.state.selectedPhoneNumber,
-      date: moment(this.state.selectedDate).format('YYYY-MM-DD'),
-      time: moment(this.state.selectedTime).format("HH:mm")
-    });
+    //Name
+    if (!this.state.selectedName) {
+      formIsValid = false;
+      errors["name"] = "Cannot be empty";
+    }
 
-    request.post({
-      headers: { 'Content-Type': 'application/json' },
-      url: 'https://apiserver-211007.appspot.com/api/appointments?access_token=11993245667',
-      body: post_data
-    }, (error, response, body) => {
-      if (response.statusCode === 200) {
-        this.setState({
-          selectedPage: 3
-        });
+    if (typeof this.state.selectedName !== "undefined") {
+      if (!this.state.selectedName.match(/^[a-zA-Z ]+$/)) {
+        formIsValid = false;
+        errors["name"] = "Only letters";
       }
-    });
+    }
+
+    //Email
+    if (!this.state.selectedEmail) {
+      formIsValid = false;
+      errors["email"] = "Cannot be empty";
+    }
+
+    if (typeof this.state.selectedEmail !== "undefined") {
+      let lastAtPos = this.state.selectedEmail.lastIndexOf('@');
+      let lastDotPos = this.state.selectedEmail.lastIndexOf('.');
+
+      if (!(lastAtPos < lastDotPos && lastAtPos > 0 && this.state.selectedEmail.indexOf('@@') == -1 && lastDotPos > 2 && (this.state.selectedEmail.length - lastDotPos) > 2)) {
+        formIsValid = false;
+        errors["email"] = "Email is not valid";
+      }
+    }
+    if (this.state.selectedPhoneNumber.length !== 10) {
+      formIsValid = false;
+      errors["phone"] = "Phone number is not valid";
+    }
+
+    this.setState({ errors: errors });
+    return formIsValid;
+  }
+
+  handleConfirmAppointment = () => {
+    if (this.handleValidation()) {
+      var post_data = JSON.stringify({
+        name: this.state.selectedName,
+        email: this.state.selectedEmail,
+        mobile: this.state.selectedPhoneNumber,
+        date: moment(this.state.selectedDate).format('YYYY-MM-DD'),
+        time: moment(this.state.selectedTime).format("HH:mm")
+      });
+
+      request.post({
+        headers: { 'Content-Type': 'application/json' },
+        url: 'https://apiserver-211007.appspot.com/api/appointments?access_token=11993245667',
+        body: post_data
+      }, (error, response, body) => {
+        if (response.statusCode === 200) {
+          this.setState({
+            selectedPage: 3
+          });
+        }
+      });
+    }
+    else {
+      console.log(this.state.errors);
+
+    }
 
 
   }
@@ -247,7 +297,7 @@ class App extends Component {
                       onChange={this.handleTimeChange}
                       showTimeSelect
                       showTimeSelectOnly
-                      minTime={moment().hours(10).minutes(0)}
+                      minTime={moment().format('YYYY-MM-DD') === this.state.selectedDate.format('YYYY-MM-DD') ? moment() : moment().hours(10).minutes(0)}
                       maxTime={moment().hours(18).minutes(0)}
                       excludeTimes={this.state.bookedSlots}
                       timeIntervals={15}
@@ -264,9 +314,14 @@ class App extends Component {
               <div>
                 <Form horizontal>
                   <FormGroup controlId="formHorizontalName">
-                    <FormControl type="text" name='name' placeholder="Full Name" onChange={(e) => this.handleNameChange(e)} /></FormGroup>
-                  <FormGroup><FormControl type="email" name='email' placeholder="Email Id" onChange={(e) => this.handleEmailChange(e)} /></FormGroup>
-                  <FormGroup><MaskedFormControl type='text' name='phoneNumber' placeholder="Phone number" mask='1111111111' onChange={(e) => this.handlePhoneNumberChange(e)} />
+                    <FormControl
+                      type="text" className={this.state.errors.name ? 'error' : ''}
+                      name='name'
+                      placeholder="Full Name"
+                      onChange={(e) => this.handleNameChange(e)}
+                    /></FormGroup>
+                  <FormGroup><FormControl type="email" className={this.state.errors.email ? 'error' : ''} name='email' placeholder="Email Id" onChange={(e) => this.handleEmailChange(e)} /></FormGroup>
+                  <FormGroup><MaskedFormControl type='text' name='phoneNumber' className={this.state.errors.phone ? 'error' : ''} placeholder="Phone number" mask='1111111111' onChange={(e) => this.handlePhoneNumberChange(e)} />
                   </FormGroup>
                   <FormGroup>
                     <Button bsStyle="success" onClick={() => this.handleConfirmAppointment()}>Confirm Appointment</Button>
